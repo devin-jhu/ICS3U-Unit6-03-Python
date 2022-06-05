9d2877328efcd25825871475ef37809308dc2d6b
Devin Jhu <devin.jhu@mths.ca>
2022-06-05 21:32:24 +0000

    code

--- any_number.py
+++ any_number.py
@@ -2,21 +2,42 @@
 
 # Created by Devin Jhu
 # Created on May 2022
-# The counter
+# The largest number finder
+
+
+import random
+
+
+def smallest(number_array):
+    # this function finds the largest number in the array
+
+    smallest_number = number_array[0]
+
+    for counter in number_array:
+        if counter < smallest_number:
+            smallest_number = counter
+
+    return smallest_number
 
 
 def main():
-    # this program counts
-    counter = 1000
+    # this function gets 10 random numbers
 
-    # process & output
-    for counter in range(1000, 2001):
-        if counter % 5 == 0:
-            print("\n{0} ".format(counter), end=" ")
-        else:
-            print("{0} ".format(counter), end=" ")
+    number_array = []
 
-    counter = counter + 1
+    print("The smallest random number finder")
+
+    # process
+    for counter in range(0, 10):
+        random_number = random.randint(0, 100)
+        number_array.append(random_number)
+        print("Random number: {}".format(random_number))
+
+    smallest_number = smallest(number_array)
+
+    # output
+    print("The smallest number is {0}.".format(smallest_number))
+
     print("\nDone.")
 
 
